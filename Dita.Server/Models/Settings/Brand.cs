58d8f2a7c6fd9c581b99355a45cfd24bb73e5163
afrowaveltd <65894408+afrowaveltd@users.noreply.github.com>
2026-04-13 22:47:02 +0200

    fixed comments

--- Dita.Server/Models/Settings/Brand.cs
+++ Dita.Server/Models/Settings/Brand.cs
@@ -1,8 +1,24 @@
 ﻿namespace Dita.Server.Models.Settings;
 
+/// <summary>
+/// The class representing the brand information for the application, including the name, logo URL, and support email.
+/// This information can be used to customize the appearance and contact details of the application for different brands
+/// or clients.
+/// </summary>
 public class Brand
 {
+   /// <summary>
+   /// Gets or sets the name associated with this instance.
+   /// </summary>
    public string Name { get; set; } = "Afrowave";
+
+   /// <summary>
+   /// Gets or sets the URL of the logo image associated with this instance.
+   /// </summary>
    public string LogoUrl { get; set; } = string.Empty;
+
+   /// <summary>
+   /// Gets or sets the support email address for contacting customer service or technical support.
+   /// </summary>
    public string SupportEmail { get; set; } = string.Empty;
 }
--- Dita.Server/Models/Settings/LocalizationSettings.cs
+++ Dita.Server/Models/Settings/LocalizationSettings.cs
@@ -1,7 +1,29 @@
 ﻿namespace Dita.Server.Models.Settings;
 
+/// <summary>
+/// Settings related to localization and language preferences for the application. This class includes properties for
+/// specifying the default language and whether to use automatic translation features, which can enhance the user
+/// experience by providing content in the user's preferred language. The automatic translation feature may require
+/// integration with a translation service such as LibreTranslate, which should be configured in the server settings for
+/// it to function properly.
+/// </summary>
 public class LocalizationSettings
 {
+   /// <summary>
+   /// Gets or sets the default language code used for localization or language-specific operations.
+   /// </summary>
+   /// <remarks>
+   /// The value should be a valid language code, such as "en" for English or "cs" for Czech. This property determines
+   /// the language context for features that support localization.
+   /// </remarks>
    public string DefaultLanguage { get; set; } = "en";
+
+   /// <summary>
+   /// Gets or sets a value indicating whether automatic translation is enabled for messages and related features.
+   /// </summary>
+   /// <remarks>
+   /// Automatic translation requires LibreTranslate to be set up and configured in the server settings. The property
+   /// value should be set to "true" to enable automatic translation, or "false" to disable it.
+   /// </remarks>
    public string UseAutomaticTranslation { get; set; } = "false";  // for messages, smart, etc.. Requires LibreTranslate to be set up and configured in the server settings
 }
