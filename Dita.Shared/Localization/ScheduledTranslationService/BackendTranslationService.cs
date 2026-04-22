9a597245c0a8d9ed91a6bb5060d4b5e6c69ec916
afrowaveltd <65894408+afrowaveltd@users.noreply.github.com>
2026-04-22 23:47:17 +0200

    translation service next step

--- Dita.Shared/Localization/ScheduledTranslationService/BackendTranslationService.cs
+++ Dita.Shared/Localization/ScheduledTranslationService/BackendTranslationService.cs
@@ -1,7 +1,26 @@
-﻿namespace Dita.Shared.Localization.ScheduledTranslationService;
+﻿using Dita.Shared.Localization.Hubs;
+using Dita.Shared.Localization.Models;
+using Dita.Shared.Localization.Services;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-public class BackendTranslationService : IBackendTranslationService
+namespace Dita.Shared.Localization.ScheduledTranslationService;
+
+public class BackendTranslationService(
+   IConfiguration configuration,
+   ILibreTranslateService translateService,
+   ILogger<BackendTranslationService> logger,
+   IHubContext<LocalizationHub> hub) : IBackendTranslationService
 {
+   private AutomaticTranslationSettings _settings => configuration
+      .GetSection("ScheduledTranslationService:AutomaticTranslation")
+      .Get<AutomaticTranslationSettings>() ?? new();
+
+   private readonly ILogger<BackendTranslationService> _logger = logger;
+   private readonly ILibreTranslateService _translateService = translateService;
+   private readonly IHubContext<LocalizationHub> _hub = hub;
+
    public async Task RunAsync()
    {
       // Implementation for running the backend translation service
